e41fe1be0c5d57f51b5e080a62c33b493b8c0cc9
Soumajit2000 <soumajitchoudhuri@gmail.com>
2021-07-03 13:47:21 +0530

    added another method using hash map

--- Hash Tables/hash_map_exercise.js
+++ Hash Tables/hash_map_exercise.js
@@ -16,8 +16,20 @@ function firstRepeating(arr) {
 	}
 }
 
+//! METHOD 2 USING HASH MAP
+function firstRepeating2(arr) {
+	let map = {};
+	for (let i = 0; i < arr.length; i++) {
+		if (map[arr[i]] !== undefined) {
+			return arr[i]
+		} else {
+			map[arr[i]] = i;
+		}
+	}
+}
+
 arr = [2, 5, 1, 2, 3, 5, 1, 2, 4];
 arr2 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
 arr3 = [1, 2, 3, 4, 5, 6, 34, 56, 23, 3, 5, 2, 1, 34, 4, 3, 6];
 arr4 = [1, 2, 3, 4, 5];
-firstRepeating(arr4);
+console.log(firstRepeating2(arr3));
