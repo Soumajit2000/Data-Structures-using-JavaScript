1fb41cbeb5e5aa3c68d14e1c28acaf8ad9c76539
Soumajit2000 <soumajitchoudhuri@gmail.com>
2021-07-06 22:18:16 +0530

    added insert and printlist function

---
+++ Linked Lists/linked_list_functions.js
@@ -0,0 +1,93 @@
+//* declaring a new node class
+class Node {
+	constructor(value) {
+		(this.value = value), (this.next = null);
+	}
+}
+
+//* declaring a new LinkedList
+class LinkedList {
+	constructor(value) {
+		this.head = {
+			value: value,
+			next: null,
+		};
+		this.tail = this.head;
+		this.length = 1;
+	}
+
+	//* adds a new node to the end of the LinkedList
+	append(value) {
+		const newNode = new Node(value);
+		this.tail.next = newNode;
+		this.tail = newNode;
+		this.length++;
+	}
+
+	//* adds a new node to the beginning of the LinkedList
+	prepend(value) {
+		const newNode = new Node(value);
+		newNode.next = this.head;
+		this.head = newNode;
+		this.length++;
+	}
+
+	//* adds a new node at an given index of the LinkedList
+	insert(index, value) {
+		const newNode = new Node(value);
+		let prev, curr;
+
+		//! Checking invalid index input
+		if (index < 0 || index > this.size || !index) {
+			console.log("Enter a valid index");
+		}
+
+        //! If the index is the last index, call append function
+		if (index === this.length) {
+			this.append(value);
+		}
+
+		//!If index is the first element, then call prepend function
+		if (index === 0) {
+			this.prepend(value);
+		}
+
+		//! adding elemetin the middle of the LinkedList
+		curr = this.head;
+		let it = 0;
+		while (it < index) {
+			it++;
+			prev = curr;
+			curr = curr.next;
+		}
+		newNode.value = value;
+		newNode.next = curr;
+		prev.next = newNode;
+		this.length++;
+	}
+
+	//*prints the LinkedList in a sequential order
+	printList() {
+		var list = " ";
+		let curr = this.head;
+		while (curr !== null) {
+			if (curr.next !== null) {
+				list += curr.value + "-->" + " ";
+			} else {
+				list += curr.value + " ";
+			}
+			curr = curr.next;
+		}
+		console.log(list);
+	}
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(21);
+myLinkedList.prepend(56);
+myLinkedList.append(39);
+myLinkedList.insert(3, 17);
+// myLinkedList.insert(87, 7);
+myLinkedList.printList();
